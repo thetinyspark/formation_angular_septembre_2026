14080958da02e7cd156519a355dc7e3e272f6397
Legrand Nicolas <n.legrand258@gmail.com>
2026-09-14 20:14:19 +0200

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+const routeConfig: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent,
+  //   title: 'Home page'
+  // }
+];
+
+export default routeConfig;
