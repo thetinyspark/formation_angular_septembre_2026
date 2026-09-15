7ab7d26c707bae6f258a9b6d9ae0069b723b2c1d
Legrand Nicolas <n.legrand258@gmail.com>
2026-09-15 10:57:54 +0200

    TP n°1 correction

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routeConfig: Routes = [
-  // {
-  //   path: '',
-  //   component: HomeComponent,
-  //   title: 'Home page'
-  // },
   {
     path: 'home',
     component: HomeComponent,
     title: 'Home page'
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+    title: 'Shop'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login page'
   }
 ];
 
