29e6cb1fda8aa21d85eeae3d6d1f5d33e5ad7a85
Legrand Nicolas <n.legrand258@gmail.com>
2026-09-17 14:55:02 +0200

    add change detector ref example

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
+  public num:number = 0;
+  private _ref = inject(ChangeDetectorRef);
+  private _ngZone = inject(NgZone);
 
+  public ngOnInit(){
+
+    this._ngZone.runOutsideAngular(
+      ()=>{
+          setInterval( 
+            ()=>{
+              if( this.num % 1000 == 0 ){
+
+                this._ngZone.runTask( 
+                  ()=>{
+                    this._ref.markForCheck();
+                  }
+                )
+
+              }
+              this.num++;
+            }, 
+            1
+          )
+      }
+    );
+
+    
+  }
 }
